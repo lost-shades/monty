4ec55135805a9d9123d0b2e04d5122ff4f1feea1
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-16 16:47:55 +0100

    new updates

--- monty.c
+++ monty.c
@@ -4,6 +4,8 @@
 instruction_t instructions[] = {
 {"push", push},
 {"pall", pall},
+{"pint", pint},
+{"pop", pop},
 {NULL, NULL}
 };
 
--- monty.h
+++ monty.h
@@ -38,6 +38,8 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void p_line(FILE *file, stack_t **stack, unsigned int *line_number,
 instruction_t *instructions);
 
